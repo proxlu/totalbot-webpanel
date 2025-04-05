0b1ab3164778d8bc64d6d161d6af304e963ade84
Eduardo Solano <105125779+proxlu@users.noreply.github.com>
2025-04-05 19:53:01 -0300

    Add files via upload

---
+++ gft/salvar/index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title>Carregando...</title>
+<link rel='shortcut icon' type='image/x-icon' href='./favicon.ico' />
+</head>
+<body bgcolor="Brown"/>
+<font color="Pink"/>
+</html>
+<?php
+header("refresh: 3;/");
+
+//PEGA OS DADOS ENVIADOS PELO FORMULÁRIO
+$output = "#!/bin/bash
+";
+
+if (isset($_POST)) {
+    foreach($_POST["totalbot"] as $numero) {
+        $output .= "./" . $numero . "
+";
+    }
+}
+
+//ARQUIVO
+$pasta = "/var/www/html/DADOS/gft";
+$arquivo = "DADOS/gft";
+
+//PREPARA O CONTEÚDO A SER GRAVADO
+if (!unlink($pasta)) {
+echo "Erro, atualizando arquivo $arquivo";
+exit;
+}
+
+//TENTA ABRIR O ARQUIVO
+if (!$abrir = fopen($pasta, "a")) {
+echo "Erro, abrindo arquivo $arquivo";
+exit;
+}
+
+//ESCREVE NO ARQUIVO
+if (!fwrite($abrir, $output)) {
+print "Erro, escrevendo arquivo $arquivo";
+exit;
+}
+
+echo "<br><center><h2>$arquivo Salvo com Sucesso!</h2></center>";
+
+//FECHA O ARQUIVO
+fclose($abrir);
+
+?>
